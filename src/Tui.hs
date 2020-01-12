3ea0112bf0e35f2528b365ed5ac3e6f09515f9f1
John Nduli <yohanaizraeli@gmail.com>
2020-01-12 14:04:56 +0300

    Add support for dynamic github urls

--- src/Tui.hs
+++ src/Tui.hs
@@ -18,6 +18,7 @@ import Cursor.Simple.List.NonEmpty
   , nonEmptyCursorSelectNext
   , nonEmptyCursorSelectPrev
   )
+import Data.List
 import qualified Data.List.NonEmpty as NE
 import Data.Maybe (catMaybes)
 import Data.Maybe
@@ -27,12 +28,51 @@ import Issues
 import System.Console.GetOpt
 import System.Environment (getArgs)
 import System.Exit (die)
+import System.Process (readProcess)
 
 data Flag
   = FileFlag String
   | UrlFlag String
   | Help
   deriving (Show, Eq)
+
+data AutoUrl
+  = AutoUrl String
+  | Fail
+  deriving (Show, Eq)
+
+-- This will automatically fail if directory is not a git repository
+getUrlFromGitRepository :: IO AutoUrl
+getUrlFromGitRepository = do
+  remote <- readProcess "git" ["remote", "-v"] []
+  let url = head . tail . words $ head $ lines remote
+  case "github" `isInfixOf` url of
+    True -> return $ AutoUrl $ formGithubUrl url
+    False -> return Fail
+
+-- /repos/:owner/:repo/issues
+-- https://api.github.com/repos/vmg/redcarpet/issues
+--
+githubIssuesUrlPrefix = "https://api.github.com/repos/" -- vmg/redcarpet/issues
+
+formGithubUrl :: String -> String
+formGithubUrl ('g':'i':'t':rest) =
+  githubIssuesUrlPrefix ++ giturl rest ++ "/issues"
+    -- assumes url is of form 'git@github.com:user/repo.git
+  where
+    giturl (':':xs) = removeGitSuffix xs
+    giturl (x:xs) = giturl xs
+formGithubUrl ('h':'t':'t':'p':rest) =
+  githubIssuesUrlPrefix ++ httpurl rest ++ "/issues"
+    -- assumes url is of form 'https://github.com/user/repo.git
+  where
+    httpurl ('m':'/':xs) = removeGitSuffix xs
+    httpurl (x:xs) = httpurl xs
+
+removeGitSuffix :: String -> String
+removeGitSuffix xs
+  | ".git" `isSuffixOf` xs = iterate init xs !! 4
+  | otherwise = xs
 
 options :: [OptDescr Flag]
 options =
@@ -61,17 +101,26 @@ tui = do
   args <- getArgs
   (actions, nonOptions) <- compilerOpts args
   -- If theres help in actions, then print usage and die
-  if Help `elem` actions
-    then die $ usageInfo "Usage: hidd [OPTION...] " options
-    else case head actions of
-           FileFlag a -> do
-             initialState <- buildInitialState a
-             endState <- defaultMain tuiApp initialState
-             print endState
-           UrlFlag a -> do
-             initialState <- buildInitialUrlState a
-             endState <- defaultMain tuiApp initialState
-             print endState
+  if null actions
+    then do
+      giturl <- getUrlFromGitRepository
+      case giturl of
+        Fail -> die "Not a git repository"
+        AutoUrl url -> urlTui url
+    else if Help `elem` actions
+           then die $ usageInfo "Usage: hidd [OPTION...] " options
+           else case head actions of
+                  FileFlag a -> do
+                    initialState <- buildInitialState a
+                    endState <- defaultMain tuiApp initialState
+                    print endState
+                  UrlFlag a -> urlTui a
+
+urlTui :: String -> IO ()
+urlTui a = do
+  initialState <- buildInitialUrlState a
+  endState <- defaultMain tuiApp initialState
+  print endState
 
 data TuiState =
   TuiState
